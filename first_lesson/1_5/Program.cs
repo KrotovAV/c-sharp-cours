32dee516d592ace8bcfbabb2a470477b5bce9ceb
KrotovAV <KrotovAV@tut.by>
2022-07-10 09:10:11 +0300

    1_5

---
+++ first_lesson/1_5/Program.cs
@@ -0,0 +1,21 @@
+﻿//5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+Console.WriteLine("Write a namber ");
+string s_a = Console.ReadLine(); // пользователь должен ввести целое число и нажать enter
+int a = int.Parse(s_a);
+
+if( a < 0 )
+{
+a = -a;
+}
+
+int n = -a;
+
+while(n <= a)
+{
+Console.Write(n);
+Console.Write(", ");
+n+=1;
+}
