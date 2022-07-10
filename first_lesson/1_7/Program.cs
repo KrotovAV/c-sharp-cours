7667c9862f8cdddca41bdc5bca6f98eccfe48285
KrotovAV <KrotovAV@tut.by>
2022-07-10 09:32:53 +0300

    1_7

---
+++ first_lesson/1_7/Program.cs
@@ -0,0 +1,14 @@
+﻿//7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+
+
+Console.WriteLine("Write a three digit number ");
+string s_a = Console.ReadLine(); // пользователь должен ввести трехзначное целое число и нажать enter
+int a = int.Parse(s_a);
+if(a < 100)
+Console.WriteLine("ERROR. The number is not three digit number!");
+if(a >= 1000)
+Console.WriteLine("ERROR. The number is not three digit number!");
+int b = a % 100;
+Console.WriteLine( b % 10);
