e2a986f0a5864054f1963b7654c1e69500c632b1
KrotovAV <KrotovAV@tut.by>
2022-07-10 06:32:28 +0300

    1_1

---
+++ first_lesson/1_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write a namer: ");
+string s_a = Console.ReadLine();// пользователь долженввести целое число и нажать enter
+int a = int.Parse(s_a);
+Console.Write("A number to the power of 2 is ");
+Console.WriteLine(a * a);
+
+
