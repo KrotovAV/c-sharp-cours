c451d46dfeb871dbbdb01054982ea04d1b81ed5c
KrotovAV <KrotovAV@tut.by>
2022-07-16 15:34:09 +0300

    start 2 lekcion

---
+++ second_lekcion/ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33
+
+int max = a1;
+
+if (b1 > max) b1 = max;
+if (c1 > max) c1 = max;
+if (a2> max) a2 = max;
+if (b2 > max) b2 = max;
+if (c2 > max) c2 = max;
+if (a3 > max) a3 = max;
+if (b3 > max) b3 = max;
+if (c3 > max) c3 = max;
+
+Console.WriteLine(max);
