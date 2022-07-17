4ffecb26635bcce593ed675f5a92846570e48727
KrotovAV <KrotovAV@tut.by>
2022-07-17 17:47:56 +0300

    2_1 done

--- second_lesson/2_1/Program.cs
+++ second_lesson/2_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+
+int SecondDigit(int a)
+{
+    return (a % 10);
+}
+
+Console.WriteLine("Write a three digit number ");
+string s_a = Console.ReadLine(); // пользователь должен ввести трехзначное целое число и нажать enter
+int a = int.Parse(s_a);
+if(a < 100)
+Console.WriteLine("ERROR. The number is not three digit number!<");
+if(a >= 1000)
+Console.WriteLine("ERROR. The number is not three digit number!>");
+
+Console.WriteLine(SecondDigit(a));
