3b9c83b32f0d697499c0f3a7479ec1e0900caf7b
KrotovAV <KrotovAV@tut.by>
2022-07-18 23:40:22 +0300

    3 lekcion sort max min

--- third_lekcion/methods/Program.cs
+++ third_lekcion/methods/Program.cs
@@ -42,19 +42,29 @@ int year = Method3();
 Console.WriteLine(year);
 
 
-// Вид  - что-то принимают и что-то возвращают
+// Вид 4 - что-то принимают и что-то возвращают
 
+//string Method4(int count, string text)
+//{
+//    int i = 0;
+//    string result = string.Empty;
+//    while(i<count)
+//    {
+//        result = result + text;
+//        i++;
+//    }
+//
+//    return result;
+//}
 string Method4(int count, string text)
 {
-    int i = 0;
-    string result = string.Empty;
-    while(i<count)
-    {
-        result = result + text;
-        i++;
-    }
+string result = string.Empty;
 
-    return result;
+for (int i = 0; i < count; i++)
+    {
+    result = result + text;
+    }
+return result;
 }
 
 string res = Method4(5, "Tex");
