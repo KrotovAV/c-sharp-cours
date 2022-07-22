63fea069c8bb89be5fcbed77e07e549d72206ad4
KrotovAV <KrotovAV@tut.by>
2022-07-22 16:32:45 +0300

    3 lesson 3_3

---
+++ third_lesson/3_3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 3: Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+void StrNamPow(int a)
+{
+    
+    for (int i = 1; i <= a; i++)
+    {
+    
+    Console.Write(Math.Pow(i, 2));
+    Console.Write(", ");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("------------");
+
+Console.WriteLine("Write a namber ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+StrNamPow(a);
+
+Console.WriteLine();
+Console.WriteLine("----------------");
+Console.WriteLine();
