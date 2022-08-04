1c68bb40385877133ddce4c937cb4b431bc5cdb7
KrotovAV <KrotovAV@tut.by>
2022-08-04 13:21:51 +0300

    fourth_lesson_HW4_3

--- fourth_lesson/4_HomeWork/HW4_3/Program.cs
+++ fourth_lesson/4_HomeWork/HW4_3/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿//Задача 3: Напишите программу, которая задаёт массив из
+//8 элементов случайными числами и выводит их на экран.
+//Оформите заполнение массива и вывод в виде функции
+//(пригодится в следующих задачах)
+
+Console.WriteLine();
+Console.WriteLine("Write array size");
+string s_n = Console.ReadLine();
+int n = int.Parse(s_n);
+
+Console.WriteLine("----------------");
+Console.Write("Array : ");
+
+void DialArray(int[] n)
+{
+    int lenght = n.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        n[i] = new Random().Next(1,1000);
+    }
+}
+
+void PrintArray(int[] n)
+{
+    int lenght = n.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write($"{n[i]}, ");
+    }
+}
+
+int[] arr = new int[n];
+
+DialArray(arr);
+PrintArray(arr);
+
+Console.WriteLine();
+Console.WriteLine();
+
+//PrintArray(arr);
