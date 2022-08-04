e92117a30b5859b5fc97303642715c5505e99165
KrotovAV <KrotovAV@tut.by>
2022-08-04 08:38:32 +0300

    fourth_lesson_HW4_1

---
+++ fourth_lesson/4_HomeWork/Hw4_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную
+//степень B.
+
+Console.WriteLine("");
+
+Console.WriteLine("Write naturele namber");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+Console.WriteLine("Write power namber");
+string s_p = Console.ReadLine();
+int p = int.Parse(s_p);
+
+Console.WriteLine("-------");
+
+int PowerMethod (int a, int p)
+{
+    int res = a;
+    for (int i = 0; i < p - 1; i++)
+    {
+        res = res * a;
+    }
+    return res;
+}
+
+Console.WriteLine($"Rowered namber: {PowerMethod(a, p)}");
+
+Console.WriteLine("");
