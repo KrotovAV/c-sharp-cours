217e57ed958e9b8d820a5a0c35f1c24cd05f00bf
KrotovAV <KrotovAV@tut.by>
2022-08-05 13:51:29 +0300

    fifth_lesson_5_2

--- fifth_lesson/5_2/Program.cs
+++ fifth_lesson/5_2/Program.cs
@@ -3,22 +3,70 @@
 //4; массив [6, 7, 19, 345, 3] -> нет
 //3; массив [6, 7, 19, 345, 3] -> да
 
-
-int[] arr = {-4, -8, 8, 2};
-int nam = 2;
-
-int[] Exist( int[] arr, int num)
+int[] DialArray(int size, int from, int to)
 {
-    for (int i = 0; i < arr.Length; i++)
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
     {
-    if(arr[i] == nam); 
-    Console.WriteLine("Namber exist in array");
-    break;
+        arr[i] = new Random().Next(from, to+1);
     }
-    
+    return arr;
 }
 
-Exist(arr, nam);
-//Console.WriteLine(Revers(arr));
+void PrintArray(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
 
-Console.WriteLine("Hello, World!");
+void FindNamber(int[] arr, int fN)
+{
+    int c = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if(fN == arr[i])
+        {
+            Console.WriteLine($"Namber {fN} exist in [{i}] array position");
+            c = c + i;
+        }
+    }
+    if(c == 0)
+        {
+        Console.WriteLine($"Namber {fN} is not exist in array");
+        }
+
+}
+
+Console.WriteLine();
+Console.WriteLine("Write array size");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write namber from");
+int f = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write namber to");
+int t = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write find namber");
+int fN = int.Parse(Console.ReadLine());
+
+Console.WriteLine("----------------");
+DialArray(n, f, t);
+
+int[] arr = DialArray(n, f, t);
+
+Console.Write("Array : ");
+PrintArray(arr);
+
+Console.WriteLine("----------------");
+FindNamber(arr, fN);
+
+Console.WriteLine();
+
+
+
+
