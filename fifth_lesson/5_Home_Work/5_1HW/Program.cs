99ab2b1aab5e1188f684e53781c6bbe8eeac0ef8
KrotovAV <KrotovAV@tut.by>
2022-08-06 06:51:56 +0300

    fifth_lesson_HW5_1

---
+++ fifth_lesson/5_Home_Work/5_1HW/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 1: Задайте массив заполненный случайными
+//положительными трёхзначными числами. Напишите
+//программу, которая покажет количество чётных чисел в
+//массиве.
+//[345, 897, 568, 234] -> 2
+
+
+
+int[] DialArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to+1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+int FindСheckPositive(int[] arr)
+{
+    int check = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]>0)
+        {
+            check = check + 1;
+        }
+    }
+    return check;
+}
+
+Console.WriteLine();
+//Console.WriteLine("Write array size");
+//int n = int.Parse(Console.ReadLine());
+int n = 10;
+
+//Console.WriteLine("Write namber from");
+//int f = int.Parse(Console.ReadLine());
+int f = -999;
+
+//Console.WriteLine("Write namber to");
+//int t = int.Parse(Console.ReadLine());
+int t = 1000;
+
+Console.WriteLine("----------------");
+DialArray(n, f, t);
+
+int[] arr = DialArray(n, f, t);
+
+Console.Write("Array : ");
+PrintArray(arr);
+
+Console.WriteLine("----------------");
+Console.Write("Positive namber: ");
+//FindСheckPositive(arr);
+Console.WriteLine(FindСheckPositive(arr));
+Console.WriteLine();
+
+
+
+
+
+
+Console.WriteLine("Hello, World!");
